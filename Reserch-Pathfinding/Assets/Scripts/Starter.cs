279fbcb98f349eb6daa7e3bb5ef96ffab0899019
harumas <haruto09.game@gmail.com>
2024-09-24 00:30:59 +0900

    init project

---
+++ Reserch-Pathfinding/Assets/Scripts/Starter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Starter : MonoBehaviour
+    {
+        [SerializeField] private Vector2[] vertices;
+        
+        private void Start()
+        {
+            var triangulator = new Triangulator();
+            triangulator.CreateInfluencePolygon(vertices);
+        }
+    }
+}
